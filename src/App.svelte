d89c01eca007a668676243b27243bc6262ce5761
studiosmith-cordell <157119820+studiosmith-cordell@users.noreply.github.com>
2025-01-27 17:34:57 +0000

    first-commit

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+    /** @type { string } */
+    let fName = $state();
+</script>
+
+<div>
+    <form action="">
+        <input
+            bind:value={fName}
+            type="text"
+            name="first-name"
+        />
+    </form>
+</div>
